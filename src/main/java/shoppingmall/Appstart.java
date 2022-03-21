31c862e0c870d479314a5072391c276c696dfe6a
오동진 <dongjin96>
2022-03-21 13:57:08 +0900

    기본셋팅

--- src/main/java/shoppingmall/Appstart.java
+++ src/main/java/shoppingmall/Appstart.java
@@ -1,4 +1,13 @@
 package shoppingmall;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing //jpa가 엔티티의 변화 감지
+@SpringBootApplication // 스프링 부트 사용 주입
 public class Appstart {
+    public static void main(String[] args) {
+        SpringBootApplication.run(Appstart.class,args);
+    }
 }
